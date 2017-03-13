46c002a1de336283d82f0504daa45bcf70aacad3
Hugo Lyra <hal@cesar.org.br>
2017-03-13 09:34:37 -0300

    Add javascript.

---
+++ assets/javascripts/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(document).scroll(function() {
+        var value = $('body').scrollTop();
+        if (value < 660) {
+            $('a[href="#events"]').removeClass('nav__item--current');
+            $('a[href="#home"]').addClass('nav__item--current');
+        }
+        if (value >= 660) {
+            $('a[href="#home"]').removeClass('nav__item--current');
+            $('a[href="#events"]').addClass('nav__item--current');
+        }
+    });
+
+    $(document).on('click', 'a.nav__item', function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    });
+});
