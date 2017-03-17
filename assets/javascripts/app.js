e9795fead56cc41a348a53c6614b9f1d854eff25
Bruno Lima <bhml@cesar.org.br>
2017-03-17 17:52:00 -0300

    WIP

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
     $(document).scroll(function() {
-        var value = $('body').scrollTop();
+        var value = $("body").scrollTop();
         if (value < 660) {
-            $('a[href="#events"]').removeClass('nav__item--current');
-            $('a[href="#home"]').addClass('nav__item--current');
+            $("a[href=#events]").removeClass("nav__item--current");
+            $("a[href=#home]").addClass("nav__item--current");
         }
         if (value >= 660) {
-            $('a[href="#home"]').removeClass('nav__item--current');
-            $('a[href="#events"]').addClass('nav__item--current');
+            $("a[href=#home]").removeClass("nav__item--current");
+            $("a[href=#events]").addClass("nav__item--current");
         }
     });
 
-    $(document).on('click', 'a.nav__item', function(event){
+    $(document).on("click", "a.nav__item", function(event){
         event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
+        $("html, body").animate({
+            scrollTop: $( $.attr(this, "href") ).offset().top
         }, 500);
     });
 
-    $(document).on('click', 'a.nav__event_item', function(event){
+    $(document).on("click", ".events-nav__item", function(event){
         event.preventDefault();
-        $('.event_year.current').toggleClass('current');
-        $($.attr(this, 'href')).toggleClass('current');
+
+        $(".events-nav__item--current").removeClass("events-nav__item--current");
+        $(this).addClass("events-nav__item--current");
+
+        $(".event_year.current").toggleClass("current");
+        $($.attr(this, "href")).toggleClass("current");
     });
 });
